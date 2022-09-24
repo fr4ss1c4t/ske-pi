d546704116c881fb9a4a320ea39bdea3604fde53
user <sre990@gmail.com>
2022-09-25 01:59:28 +0200

    fixed the sequential stream in, renamed some variables

--- example/test_mapred.erl
+++ example/test_mapred.erl
@@ -2,34 +2,32 @@
 -export([benchmark/0,benchmark/3]).
 -import('math',[sin/1,pow/2,log2/1]).
 
-%TODO: QUELLA ATTUALE LA RINOMINO pmapred_naive-NAIVE e la confronto anche con una
-% altra che utilizzi una una parallel fold(tipo skel o plists)
-
 % testing the mapreduce skeleton by applying the function
 % fn=1+(sin(X))^Exp, where X is a random number from 0 to 100
 
 % default configuration
 benchmark() ->
-   Schedulers_num = utils:get_schedulers(),
+   Schedulers_Num = utils:get_schedulers(),
    Exp = 20,
-   Chunks_exp = Exp-round(log2(Schedulers_num)),
-   benchmark(Exp,Chunks_exp,Schedulers_num).
+   Chunks_Exp = Exp-round(log2(Schedulers_Num)),
+   benchmark(Exp,Chunks_Exp,Schedulers_Num).
 % customise length of list, length of chunks and number of
 % schedulers online
-benchmark(Exp,Chunks_exp,Schedulers_num) ->
-   utils:set_schedulers(Schedulers_num),
+benchmark(Exp,Chunks_Exp,Schedulers_Num) ->
+   utils:set_schedulers(Schedulers_Num),
    List = [rand:uniform(100)|| _ <- utils:create_list(Exp)],
 
-   io:format("testing with ~w scheduler(s)~n", [Schedulers_num]),
+   io:format("testing with ~w scheduler(s)~n", [Schedulers_Num]),
+   io:format("the list is 2^~w=~w elements long~n",[Exp,length(List)]),
    if
-      Exp>Chunks_exp ->
-         Chunks_len = round(pow(2,Chunks_exp)),
+      Exp>Chunks_Exp ->
+         Chunks_Len = round(pow(2,Chunks_Exp)),
          io:format("2^~w chunks of length 2^~w=~w~n",
-         [Exp-Chunks_exp, Chunks_exp,Chunks_len]);
+         [Exp-Chunks_Exp, Chunks_Exp,Chunks_Len]);
       true ->
-         Chunks_len = round(pow(2,Exp)),
+         Chunks_Len = round(pow(2,Exp)),
          io:format("2^~w chunks of length 2^~w=~w~n",
-         [0, Exp,Chunks_len])
+         [0, Exp,Chunks_Len])
    end,
 
    % sequential version using a fold and a map
@@ -39,41 +37,41 @@ benchmark(Exp,Chunks_exp,Schedulers_num) ->
             1 + pow(sin(X),Exp) end,List)) end,
 
    % parallel version
-   P_mapred = fun() ->
+   P_MapRed = fun() ->
       pmapred_naive:start(
       fun(Chunk)->lists:sum([1+pow(sin(X),Exp)||X<-Chunk]) end,
-      utils:make_chunks(Chunks_len,List)) end,
+      utils:make_chunks(Chunks_Len,List)) end,
 
    % parallel version with pre-partitioned data
-   Chunks = utils:make_chunks(Chunks_len,List),
-   C_mapred = fun() ->
+   Chunks = utils:make_chunks(Chunks_Len,List),
+   C_MapRed = fun() ->
       pmapred_naive:start(
       fun(Chunk)-> lists:sum([1+pow(sin(X),Exp)||X<-Chunk]) end, Chunks) end,
 
 
-   Time_seq = utils:test_loop(12,Seq, []),
-   Mean_seq = utils:mean(Time_seq),
-   Median_seq = utils:median(Time_seq),
-   Time_p = utils:test_loop(12,P_mapred, []),
-   Mean_p = utils:mean(Time_p),
-   Median_p = utils:median(Time_p),
-   Time_c = utils:test_loop(12,C_mapred, []),
-   Mean_c = utils:mean(Time_c),
-   Median_c = utils:median(Time_c),
-   Speedup1 = utils:speedup(Mean_seq,Mean_p),
-   Speedup2 = utils:speedup(Mean_seq,Mean_c),
+   Time_Seq = utils:test_loop(12,Seq, []),
+   Mean_Seq = utils:mean(Time_Seq),
+   Median_Seq = utils:median(Time_Seq),
+   Time_Par = utils:test_loop(12,P_MapRed, []),
+   Mean_Par = utils:mean(Time_Par),
+   Median_Par = utils:median(Time_Par),
+   Time_Ch = utils:test_loop(12,C_MapRed, []),
+   Mean_Ch = utils:mean(Time_Ch),
+   Median_Ch = utils:median(Time_Ch),
+   Speedup_Par = utils:speedup(Mean_Seq,Mean_Par),
+   Speedup_Ch = utils:speedup(Mean_Seq,Mean_Ch),
 
    io:format("sequential version mean is"),
    io:format(" ~wms, whilst median is ~wms~n",
-      [Mean_seq/1000,Median_seq/1000]),
+      [Mean_Seq/1000,Median_Seq/1000]),
 
    io:format("naive parallel version mean is"),
    io:format(" ~wms, whilst median is ~wms~n",
-      [Mean_p/1000,Median_p/1000]),
+      [Mean_Par/1000,Median_Par/1000]),
 
    io:format("pre-partitioned version is"),
    io:format(" ~wms, whilst median is ~wms~n",
-             [Mean_c/1000,Median_c/1000]),
+             [Mean_Ch/1000,Median_Ch/1000]),
 
-   io:format("speed up of naive parallel version is ~w~n", [Speedup1]),
-   io:format("speed up of pre-partitioned version is ~w~n", [Speedup2]).
+   io:format("speed up of naive parallel version is ~w~n", [Speedup_Par]),
+   io:format("speed up of pre-partitioned version is ~w~n", [Speedup_Ch]).
--- example/test_stream.erl
+++ example/test_stream.erl
@@ -7,80 +7,80 @@
 
 % default configuration
 benchmark() ->
-Schedulers_num = utils:get_schedulers(),
-Exp = 20,
-Chunks_exp = Exp-round(log2(Schedulers_num)),
-benchmark(Exp,Chunks_exp,Schedulers_num,Schedulers_num).
+   Schedulers_Num = utils:get_schedulers(),
+   Exp = 20,
+   Chunks_Exp = Exp-round(log2(Schedulers_Num)),
+   benchmark(Exp,Chunks_Exp,Schedulers_Num,Schedulers_Num).
 % customise length of list, length of chunks, number of workers
 % and schedulers online
-benchmark(Exp,Chunks_exp,W,Schedulers_num) ->
-   utils:set_schedulers(Schedulers_num),
+benchmark(Exp,Chunks_Exp,W,Schedulers_Num) ->
+   utils:set_schedulers(Schedulers_Num),
    List = [rand:uniform(100)||
    _ <- utils:create_list(Exp)],
 
 
    io:format("testing with ~w scheduler(s) and ~w worker(s)~n",
-   [Schedulers_num,W]),
+   [Schedulers_Num,W]),
+   io:format("the list is 2^~w=~w elements long~n",[Exp,length(List)]),
    if
-      Exp>Chunks_exp ->
-         Chunks_len = round(pow(2,Chunks_exp)),
+      Exp>Chunks_Exp ->
+         Chunks_Len = round(pow(2,Chunks_Exp)),
          io:format("2^~w chunks of length 2^~w=~w~n",
-         [Exp-Chunks_exp, Chunks_exp,Chunks_len]);
+         [Exp-Chunks_Exp, Chunks_Exp,Chunks_Len]);
       true ->
-         Chunks_len = round(pow(2,Exp)),
+         Chunks_Len = round(pow(2,Exp)),
          io:format("2^~w chunks of length 2^~w=~w~n",
-         [0, Exp,Chunks_len])
+         [0, Exp,Chunks_Len])
    end,
 
-   M_func = fun(Input) ->
+   Fun = fun(Input) ->
       [1 + pow(sin(X),Exp) || X <- Input]
    end,
 
-   W_func = fun(Chunks) ->
+   W_Fun = fun(Chunks) ->
       lists:sum(
       [1 + pow(sin(X),Exp)
       || X <- Chunks])
    end,
 
-   % sequential version using a fold and a map
-   Seq = fun() ->
-      lists:foldl((fun(X,Sum) ->
-         X+Sum end),0,lists:map(fun(X)->
-            1 + pow(sin(X),Exp) end,List)) end,
+   % sequential version is a farm with only one worker
+   Seq =
+      fun() ->
+         stream:start_farm(1,W_Fun, utils:make_chunks(Chunks_Len,List))
+      end,
 
    % pipeline version with two stages
    Pipe =
       fun() ->
-         lists:sum(
          stream:start_pipe(
-         [M_func, fun lists:sum/1],
-            utils:make_chunks(Chunks_len,List))) end,
+         [Fun, fun lists:sum/1],
+            utils:make_chunks(Chunks_Len,List))
+      end,
 
    % farm version
    Farm =
-   fun() ->
-      lists:sum(
-      stream:start_farm(
-      W,W_func,
-      utils:make_chunks(Chunks_len,List))) end,
+      fun() ->
+         stream:start_farm(W,W_Fun,
+         utils:make_chunks(Chunks_Len,List))
+      end,
 
-      Time_seq = utils:test_loop(12,Seq, []),
-      Mean_seq = utils:mean(Time_seq),
-      Median_seq = utils:median(Time_seq),
-      Time_pipe = utils:test_loop(12,Pipe, []),
-      Mean_pipe = utils:mean(Time_pipe),
-      Median_pipe = utils:median(Time_pipe),
-      Time_farm = utils:test_loop(12,Farm, []),
-      Mean_farm = utils:mean(Time_farm),
-      Median_farm = utils:median(Time_farm),
-      Speedup1 = utils:speedup(Mean_seq,Mean_pipe),
-      Speedup2 = utils:speedup(Mean_seq,Mean_farm),
+      Time_Seq = utils:test_loop(12,Seq, []),
+      Mean_Seq = utils:mean(Time_Seq),
+      Median_Seq = utils:median(Time_Seq),
+      Time_Pipe = utils:test_loop(12,Pipe, []),
+      Mean_Pipe = utils:mean(Time_Pipe),
+      Median_Pipe = utils:median(Time_Pipe),
+      Time_Farm = utils:test_loop(12,Farm, []),
+      Mean_Farm = utils:mean(Time_Farm),
+      Median_Farm = utils:median(Time_Farm),
+      Speedup_Pipe = utils:speedup(Mean_Seq,Mean_Pipe),
+      Speedup_Farm = utils:speedup(Mean_Seq,Mean_Farm),
 
       io:format("sequential mean is ~wms, whilst median is ~wms~n",
-      [Mean_seq/1000,Median_seq/1000]),
+      [Mean_Seq/1000,Median_Seq/1000]),
       io:format("pipe mean is ~wms, whilst median is ~wms~n",
-      [Mean_pipe/1000,Median_pipe/1000]),
+      [Mean_Pipe/1000,Median_Pipe/1000]),
       io:format("farm mean is ~wms, whilst median is ~wms~n",
-      [Mean_farm/1000,Median_farm/1000]),
-      io:format("speedup for pipe is ~w~n",[Speedup1]),
-      io:format("speedup for farm is ~w~n", [Speedup2]).
+      [Mean_Farm/1000,Median_Farm/1000]),
+      io:format("speedup for pipe is ~w~n",[Speedup_Pipe]),
+      io:format("speedup for farm is ~w~n", [Speedup_Farm]).
